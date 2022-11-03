202e69e8e736d77ef229a8109c0223b85dc94a53
Jedd Saliba <jeddsaliba@gmail.com>
2022-11-04 07:30:17 +0800

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -3,12 +3,9 @@
 namespace JSCustom\LaravelImageResize\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use JSCustom\LaravelImageResize\Helpers\ImageResize;
 
 class Controller extends BaseController
 {
-    public function __construct(
-        \JSCustom\LaravelImageResize\Helpers\ImageResize $ImageResize
-    ) {
-        $this->_imageResize = $ImageResize;
-    }
+    public function __construct() {}
 }
